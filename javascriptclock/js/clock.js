9347892eac7cda039a47b4906c03aafad2f46535
Eralp <eralpozcans@gmail.com>
2021-09-17 00:17:00 +0300

    JS Ödev1

---
+++ javascriptclock/js/clock.js
@@ -0,0 +1,29 @@
+
+
+
+let userName = prompt("Lütfen isminizi giriniz")
+
+let nameData = document.querySelector("#myName");
+nameData.innerHTML = `${userName}`
+
+
+let clocked = document.querySelector("#myClock");
+
+function showTime(){
+    const today = new Date();
+    let hour = today.getHours();
+    let min = today.getMinutes();
+    let second = today.getSeconds();
+    let day = today.getDay();
+    let dList = ["Pazartesi","Salı","Çarşamba","Perşembe","Cuma","Cumartesi","Pazar"];
+
+    hour = hour<10 ? "0"+ hour: hour;
+    min = min <10 ? "0" + min :min;
+    second = second<10 ? "0"+second : second;
+    //clock.innerHTML = `test`
+    clocked.innerHTML=`${hour}:${min}:${second} ${dList[day]}`;
+    setTimeout(showTime,1000);
+}
+
+
+showTime();
